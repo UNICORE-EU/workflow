b2b9d3749ba739abb981c7bae30ba0701d20ff64
Bernd Schuller <b.schuller@fz-juelich.de>
2022-07-08 10:37:19 +0200

    .

--- workflow-server/src/main/java/eu/unicore/workflow/pe/util/ClientConfigProvider.java
+++ workflow-server/src/main/java/eu/unicore/workflow/pe/util/ClientConfigProvider.java
@@ -1,7 +1,6 @@
 package eu.unicore.workflow.pe.util;
 
 import eu.unicore.security.wsutil.client.authn.ClientConfigurationProviderImpl;
-import eu.unicore.security.wsutil.client.authn.DelegationSpecification;
 import eu.unicore.util.httpclient.IClientConfiguration;
 
 /**
@@ -14,21 +13,9 @@ public class ClientConfigProvider extends ClientConfigurationProviderImpl {
 		super();
 		setBasicConfiguration(baseConfig);
 	}
-	
-	@Override
-	public IClientConfiguration getClientConfiguration(String serviceUrl, String serviceIdentity, 
-			DelegationSpecification delegate) throws Exception
-	{
-		if (serviceUrl == null)
-			throw new IllegalArgumentException("Service URL must be always given");
-	
-		return getBasicClientConfiguration().clone();
-	}
-	
+
 	public IClientConfiguration getClientConfiguration(String url) {
-		if (url == null)
-			throw new IllegalArgumentException("Service URL must be always given");
-	
-		return getBasicClientConfiguration().clone(); 
+		if (url == null)throw new IllegalArgumentException("Service URL must be always given");
+		return getBasicClientConfiguration().clone();
 	}
 }
